06f9a191b043fcc9e278ddccb4b39adf59f77ece
Joelle Helm <joellenhelm@gmail.com>
2019-08-26 23:20:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -82,9 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-		 programmer_hash
+		 programmer_hash.unshift(:yukihiro_matsumoto => {})
+
 
 end
 
